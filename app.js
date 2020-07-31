c218f504b9ab3cb54341a2e869ad1de1f902643e
shivanichauhan18 <shivanic18@navgurukul.org>
2020-07-31 12:31:15 +0530

    created api with moongose

--- app.js
+++ app.js
@@ -1,5 +1,21 @@
 var express = require('express')
+
+var app = express()
 var path = require('path')
+var favicon = require('serve-favicon')
+var logger = require('morgan')
+var cookieParser = require('cookie-parser')
 var bodyParser = require('body-parser')
-var db = require('./config/connection')
-var blob = require("./model/blobs")
+
+var db = require("./model/db")
+var blob = require('./model/blobs');
+
+var blobs = require('./routes/blobs');
+app.use('/blobs', blobs);
+
+
+
+
+app.listen(3500, () => {
+    console.log("server started on port 3500")
+})
